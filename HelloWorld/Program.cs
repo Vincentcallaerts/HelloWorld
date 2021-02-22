1bdcc0d8d0599913a6ca50f8e960f1449198a3a8
Vincentcallaerts <57138252+Fattunatunatuna@users.noreply.github.com>
2021-02-22 15:08:44 +0100

    5 verschillende manieren van Write/Writeline

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,27 +6,32 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            //variabels
             string naam;
             string mood;
             string heleZin;
-
+            
+            //start
             Console.WriteLine("Hello world!");
             Console.WriteLine("Wie ben jij ?");
-           
+
             naam = Console.ReadLine();
             Console.WriteLine("Hallo " + naam);
             Console.WriteLine("Hoe gaat het met jouw ");
             mood = Console.ReadLine();
-
+           
+            //eerste manier
             Console.Write("Met mij gaat het ook ");
             Console.Write(mood);
             Console.WriteLine(".");
 
+            //tweede manier
             Console.WriteLine("Met mij gaat het ook " + mood);
-           
+            //derde manier 
             Console.WriteLine("Met mij gaat het ook {0} ", mood);
+            //vierde manier
             Console.WriteLine($"Met mij gaat het ook {mood}");
-
+            //vijfde manier
             heleZin = $"Met mij gaat het ook {mood}.";
             Console.WriteLine(heleZin);
 
